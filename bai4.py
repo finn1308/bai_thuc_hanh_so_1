a7c56a9ad9907ae8260e99618a62fb789ee5b088
Nguyen Huy <nguyenvanhuy13122008@gmail.com>
2026-09-23 16:57:26 +0700

    Create bai4.py

---
+++ bai4.py
@@ -0,0 +1,5 @@
+Toan = float(input(" Nhập điểm môn Toán: "))
+Van = float(input(" Nhập điểm môn Văn: "))
+Anh = float(input(" Nhập điểm môn Anh: "))
+TB = (Toan + Van + Anh)/3
+print(f" Điểm trung bình = {TB:.2f}")
