430afafef83fd3999a2a9a53086b77888f691fd7
Nguyen Huy <nguyenvanhuy13122008@gmail.com>
2026-09-23 16:50:13 +0700

    Add user input and greeting in bai1.py

---
+++ bai1.py
@@ -0,0 +1,4 @@
+name = input(" Bạn tên là gì ?")
+age = int(input("Bạn bao nhiêu tuổi ? "))
+school = input(" Bạn học trường nào ?")
+print (f" Xin chào, tôi tên là {name}, năm nay {age}, đang học tại {school}")
