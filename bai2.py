6652f37d0a68be58df8ce581ef6be2a25c91e752
Nguyen Huy <nguyenvanhuy13122008@gmail.com>
2026-09-23 16:52:47 +0700

    Add rectangle perimeter and area calculations

---
+++ bai2.py
@@ -0,0 +1,4 @@
+dai = int(input(" Chiều dài của hình chữ nhật: "))
+rong = int(input(" Chiều rộng của hình chữ nhật: "))
+print (f" Chu vi của hình chữ nhật = {(dai + rong) * 2}")
+print (f" Diện tích của hình chữ nhật = {dai * rong}")
